5d6e15e875680ed7abce23c7029fcc751cd489db
Adam G Horn <hornadam8@gmail.com>
2020-01-30 15:43:50 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,4 +2,27 @@ class Song
   
   attr_accessor :song :artist :genre
   
-  def initialize
+  @@count = 0
+  @@genres = []
+  @@artists = []
+  
+  def initialize(song,artist,genre)
+    @@count += 1
+    unless @@genres.include?(genre)
+      @@genres << genre
+    end
+    unless @@artists.include?(artist)
+      @@artists << artist
+    end
+  end
+  
+  def count
+    @@count
+  end
+  
+  def genres 
+    @@genres
+  end
+  
+  def genre_count
+    
